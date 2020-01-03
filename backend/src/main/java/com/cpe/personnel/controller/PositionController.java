e3131f9ad868792d31f069912c412118d09cbfc8
sopon-ph <soponsoio123456@gmail.com>
2020-01-03 22:57:25 +0700

    add all controller to backend - close #26

---
+++ backend/src/main/java/com/cpe/personnel/controller/PositionController.java
@@ -0,0 +1,24 @@
+package com.cpe.personnel.controller;
+import com.cpe.personnel.entity.Position;
+import com.cpe.personnel.repository.PositionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class PositionController {
+
+    @Autowired
+    private final PositionRepository positionRepository;
+    public PositionController(PositionRepository  positionRepository) {
+        this. positionRepository =  positionRepository;
+    }
+    @GetMapping("/position")
+    public Collection<Position> Positions() {
+        return  positionRepository.findAll().stream().collect(Collectors.toList());
+    }
+}
