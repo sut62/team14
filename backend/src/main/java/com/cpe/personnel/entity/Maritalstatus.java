328a8c394e2e3c0e6f283206a3cf17324be30287
sopon-ph <soponsoio123456@gmail.com>
2020-01-03 22:22:03 +0700

    move Maritalstatus.java to Backend - close #49

---
+++ backend/src/main/java/com/cpe/personnel/entity/Maritalstatus.java
@@ -0,0 +1,28 @@
+package com.cpe.personnel.entity;
+import lombok.*;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import java.util.Collection;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name="MARITALSTATUS")
+public class Maritalstatus {
+
+    @Id
+    @SequenceGenerator(name="MARITALSTATUS_SEQ",sequenceName="MARITALSTATUS_SEQ")               
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="MARITALSTATUS_SEQ")  
+    @Column(name="MARITALSTATUS_ID", unique = true, nullable = true)
+    private @NonNull Long id;
+    private @NonNull String maritalstatus_name;
+    @OneToMany(fetch = FetchType.EAGER)
+    private Collection<Personnel> personnel;
+}
