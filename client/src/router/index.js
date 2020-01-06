e2d31c23fa6d6ed18a3e4b72f881f312f34d3228
Tanatat <tanatattry@gmail.com>
2020-01-06 21:20:45 +0700

    close #32 add UI Food system

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router';
 import Personnel from '../components/Personnel';
-<<<<<<< HEAD
 import order from '../components/FoodOrder';
-=======
 import Food from '../components/Food';
 
->>>>>>> close #32 add UI for food system
 
 Vue.use(Router);
 
@@ -18,15 +15,12 @@ export default new Router({
             component: Personnel
         },
         {
-<<<<<<< HEAD
              path: '/order',
              component: order
-        }
+        },{
         
-=======
             path: '/Food',
             component: Food
         },
->>>>>>> close #32 add UI for food system
     ]
 });
