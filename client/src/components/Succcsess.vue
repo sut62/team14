18ab18f6b5982026b0cf81c82dafbfeaf7eaf1e7
Tanatat <tanatattry@gmail.com>
2020-01-10 14:13:54 +0700

    add successgit add .

---
+++ client/src/components/Succcsess.vue
@@ -0,0 +1,21 @@
+<template  >
+    
+            <v-container    >
+              <v-row justify>   
+                 <v-col cols="20"  >
+              <v-layout text-center wrap >
+                <v-flex mb-0.1>
+                  <label
+                    class="font-weight-black  blue-grey--text  text-uppercas text-uppercas"
+                    style="font-size:4em  "
+                  >successful!! </label>
+                  <br>
+                    
+                </v-flex>
+              </v-layout>
+                 </v-col>
+              </v-row>
+
+            </v-container>
+    
+</template>
