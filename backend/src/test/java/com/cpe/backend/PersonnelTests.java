3395fd7789e7cd5f29ce571f72af6e41ec1ff5b1
sopon-ph <soponsoio123456@gmail.com>
2020-01-25 18:39:07 +0700

    adjunct code TestCase Entity Personnel - close #100

--- backend/src/test/java/com/cpe/backend/PersonnelTests.java
+++ backend/src/test/java/com/cpe/backend/PersonnelTests.java
@@ -1,7 +1,7 @@
 package com.cpe.backend;
 
-import com.cpe.personnel.entity.Personnel;
-import com.cpe.personnel.repository.PersonnelRepository;
+import com.cpe.personnel.entity.*;
+import com.cpe.personnel.repository.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +28,12 @@ public class PersonnelTests {
 
     @Autowired
     private PersonnelRepository personnelRepository;
+    @Autowired
+    private EducationlevelRepository educationlevelRepository;
+    @Autowired
+    private MaritalstatusRepository maritalstatusRepository;
+    @Autowired
+    private PositionRepository positionRepository;
 
     @BeforeEach
     public void setup() {
@@ -38,13 +44,29 @@ public class PersonnelTests {
     // ตั้งชื่อ test ให้สอดคล้องกับสิ่งที่ต้อง test
 
     @Test
-    void B6024321_testPersonnelAllDataNotNull() {
+    void B6024321_testPersonnelAllDataComplete() {
+
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+        
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);
+
         Personnel personnel = new Personnel();
         personnel.setFirstname("sopon");
         personnel.setLastname("phudee");
         personnel.setTelephone("0856845611");
         personnel.setPassword("12345648790");
         personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
         personnel = personnelRepository.saveAndFlush(personnel);
 
         Optional<Personnel> found = personnelRepository.findById(personnel.getId());
@@ -52,16 +74,35 @@ public class PersonnelTests {
         assertEquals("phudee", found.get().getLastname());
         assertEquals("0856845611", found.get().getTelephone());
         assertEquals("12345648790", found.get().getPassword());
+        assertEquals(LocalDate.parse("1999-01-29"), found.get().getBirthday());
+        assertEquals(status, found.get().getStatus());
+        assertEquals(posit, found.get().getPosit());
+        assertEquals(level, found.get().getLevel());
         
     }
     @Test
     void B6024321_testLastnameMustNotBeNull() {
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+        
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);
+
         Personnel personnel = new Personnel();
         personnel.setFirstname("sopon");
         personnel.setLastname(null);
         personnel.setTelephone("0856845611");
         personnel.setPassword("12345648790");
         personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
 
         Set<ConstraintViolation<Personnel>> result = validator.validate(personnel);
 
@@ -74,13 +115,28 @@ public class PersonnelTests {
         assertEquals("lastname", v.getPropertyPath().toString());
     }
     @Test
-    void B6024321_testPersonnelIdMustNotBe11Digits() {
+    void B6024321_testTelephoneMustNotBe11Digits() {
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+        
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);
+
         Personnel personnel = new Personnel();
         personnel.setFirstname("sopon");
         personnel.setLastname("phudee");
         personnel.setTelephone("08568456111");
         personnel.setPassword("12345648790");
         personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
 
         Set<ConstraintViolation<Personnel>> result = validator.validate(personnel);
 
@@ -94,12 +150,27 @@ public class PersonnelTests {
     }
     @Test
     void B6024321_testTelephoneMustNotBe9Digits() {
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+        
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);
+
         Personnel personnel = new Personnel();
         personnel.setFirstname("sopon");
         personnel.setLastname("phudee");
         personnel.setTelephone("085684561");
         personnel.setPassword("12345648790");
         personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
 
         Set<ConstraintViolation<Personnel>> result = validator.validate(personnel);
 
@@ -112,7 +183,20 @@ public class PersonnelTests {
         assertEquals("telephone", v.getPropertyPath().toString());
     }
     @Test
-    void B6024321_testLocalTimeDateMustNotBePresent(){
+    void B6024321_testLocalDateMustNotBePresent(){
+        
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+        
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);
+
         Personnel personnel = new Personnel();
         LocalDate date = LocalDate.now();
         personnel.setFirstname("sopon");
@@ -120,6 +204,9 @@ public class PersonnelTests {
         personnel.setTelephone("0856845611");
         personnel.setPassword("12345648790");
         personnel.setBirthday(date);
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
 
         Set<ConstraintViolation<Personnel>> result = validator.validate(personnel);
 
@@ -129,6 +216,147 @@ public class PersonnelTests {
         ConstraintViolation<Personnel> vi = result.iterator().next();
         assertEquals("must be a past date", vi.getMessage());
         assertEquals("Birthday", vi.getPropertyPath().toString());
+    }
+    @Test
+    void B6024321_testLocalDateMustNotBefuture(){
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+        
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);
+
+        Personnel personnel = new Personnel();
+        personnel.setFirstname("sopon");
+        personnel.setLastname("phudee");
+        personnel.setTelephone("0856845611");
+        personnel.setPassword("12345648790");
+        personnel.setBirthday(LocalDate.parse("2592-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
+
+        Set<ConstraintViolation<Personnel>> result = validator.validate(personnel);
+
+        //must has 1 error
+        assertEquals(1, result.size());
+        //error message and path must be correct
+        ConstraintViolation<Personnel> vi = result.iterator().next();
+        assertEquals("must be a past date", vi.getMessage());
+        assertEquals("Birthday", vi.getPropertyPath().toString());
+    }
+    @Test
+    void B6024321_testEducationlevelMustNotBeNull() { 
+        
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+        
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);
+
+        
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Personnel personnel = new Personnel();
+        personnel.setFirstname("sopon");
+        personnel.setLastname("phudee");
+        personnel.setTelephone("0856845611");
+        personnel.setPassword("12345648790");
+        personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(null);
+
+        Set<ConstraintViolation<Personnel>> result = validator.validate(personnel);
+
+        // result ต้องมี error 1 ค่าเท่านั้น
+        assertEquals(1, result.size());
+
+        // error message ตรงชนิด และถูก field
+        ConstraintViolation<Personnel> v = result.iterator().next();
+        assertEquals("must not be null", v.getMessage());
+        assertEquals("level", v.getPropertyPath().toString());
+    }
+    @Test
+    void B6024321_testMaritalstatusMustNotBeNull() { 
+        
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+        
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);
+
+        
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Personnel personnel = new Personnel();
+        personnel.setFirstname("sopon");
+        personnel.setLastname("phudee");
+        personnel.setTelephone("0856845611");
+        personnel.setPassword("12345648790");
+        personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(null);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
+
+        Set<ConstraintViolation<Personnel>> result = validator.validate(personnel);
+
+        // result ต้องมี error 1 ค่าเท่านั้น
+        assertEquals(1, result.size());
+
+        // error message ตรงชนิด และถูก field
+        ConstraintViolation<Personnel> v = result.iterator().next();
+        assertEquals("must not be null", v.getMessage());
+        assertEquals("status", v.getPropertyPath().toString());
+    }
+    @Test
+    void B6024321_testPositionMustNotBeNull() { 
+        
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+        
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);
+
+        
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Personnel personnel = new Personnel();
+        personnel.setFirstname("sopon");
+        personnel.setLastname("phudee");
+        personnel.setTelephone("0856845611");
+        personnel.setPassword("12345648790");
+        personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(null);
+        personnel.setLevel(level);
+
+        Set<ConstraintViolation<Personnel>> result = validator.validate(personnel);
+
+        // result ต้องมี error 1 ค่าเท่านั้น
+        assertEquals(1, result.size());
+
+        // error message ตรงชนิด และถูก field
+        ConstraintViolation<Personnel> v = result.iterator().next();
+        assertEquals("must not be null", v.getMessage());
+        assertEquals("posit", v.getPropertyPath().toString());
     }
 }
 
