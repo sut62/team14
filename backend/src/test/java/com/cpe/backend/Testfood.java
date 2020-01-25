5b2ef26ca21b195db9223b09b01d4f0a5d429b2d
Tanatat <tanatattry@gmail.com>
2020-01-26 02:30:51 +0700

    close #102 add code testcase

--- backend/src/test/java/com/cpe/backend/Testfood.java
+++ backend/src/test/java/com/cpe/backend/Testfood.java
@@ -1,7 +1,9 @@
 package com.cpe.backend;
 
-import com.cpe.backendfood.entity.Food;
-import com.cpe.backendfood.repository.FoodRepository;
+import com.cpe.backendfood.entity.*;
+import com.cpe.backendfood.repository.*;
+import com.cpe.personnel.entity.*;
+import com.cpe.personnel.repository.*;
 
 
 import org.junit.jupiter.api.BeforeEach;
@@ -17,6 +19,8 @@ import javax.validation.ValidatorFactory;
 import java.util.Optional;
 import java.util.Set;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import javax.validation.constraints.NotBlank;
@@ -30,6 +34,19 @@ public class Testfood {
 
     @Autowired
     private FoodRepository foodRepository;
+    @Autowired
+    private FoodtypeRepository foodtypeRepository;
+    @Autowired
+    private MealRepository mealRepository;
+    @Autowired
+    private PersonnelRepository personnelRepository;
+
+    @Autowired
+    private EducationlevelRepository educationlevelRepository;
+    @Autowired
+    private MaritalstatusRepository maritalstatusRepository;
+    @Autowired
+    private PositionRepository positionRepository;
 
     @BeforeEach
     public void setup() {
@@ -39,21 +56,97 @@ public class Testfood {
 
     // ตั้งชื่อ test ให้สอดคล้องกับสิ่งที่ต้อง test
      @Test
-    void B6021726_testFoodOKWithAllDataNotNull() {
+    void B6021726_testFoodAllDataComplete() {
+
+        Foodtype type = new Foodtype();
+        type.setType("อาหารธรรมดา");
+        type = foodtypeRepository.saveAndFlush(type);
+
+        Meal meal = new Meal();
+        meal.setMealtype("เช้า");
+        meal = mealRepository.saveAndFlush(meal);
+
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);        
+
+        Personnel personnel = new Personnel();
+        personnel.setFirstname("sopon");
+        personnel.setLastname("phudee");
+        personnel.setTelephone("0856845611");
+        personnel.setPassword("12345648790");
+        personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
+        personnel = personnelRepository.saveAndFlush(personnel);
+
+
         Food name = new Food();
         name.setName("ข้าวต้ม");
+        name.setCreatedby(personnel);
+        name.setTypeby(type);
+        name.setMealby(meal);
         name = foodRepository.saveAndFlush(name);
 
         // error message ตรงชนิด และถูก field
         Optional<Food> found = foodRepository.findById(name.getId());
         assertEquals("ข้าวต้ม", found.get().getName());
+        assertEquals(type, found.get().getTypeby());
+        assertEquals(meal, found.get().getMealby());
+
+        assertEquals(personnel, found.get().getCreatedby());
     }
 
 
     @Test
     void b6021726_testFoodOKWithNotbeNull() {
+
+        Foodtype type = new Foodtype();
+        type.setType("อาหารธรรมดา");
+        type = foodtypeRepository.saveAndFlush(type);
+
+        Meal meal = new Meal();
+        meal.setMealtype("เช้า");
+        meal = mealRepository.saveAndFlush(meal);
+
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);        
+
+        Personnel personnel = new Personnel();
+        personnel.setFirstname("sopon");
+        personnel.setLastname("phudee");
+        personnel.setTelephone("0856845611");
+        personnel.setPassword("12345648790");
+        personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
+        personnel = personnelRepository.saveAndFlush(personnel);
+        
+
         Food name = new Food();
         name.setName(null);
+        name.setCreatedby(personnel);
+        name.setTypeby(type);
+        name.setMealby(meal);
 
         Set<ConstraintViolation<Food>> result = validator.validate(name);
         assertEquals(1, result.size());
@@ -64,8 +157,44 @@ public class Testfood {
     }
     @Test
     void B6021726_testSize3() {
+
+        Foodtype type = new Foodtype();
+        type.setType("อาหารธรรมดา");
+        type = foodtypeRepository.saveAndFlush(type);
+
+        Meal meal = new Meal();
+        meal.setMealtype("เช้า");
+        meal = mealRepository.saveAndFlush(meal);
+
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);        
+
+        Personnel personnel = new Personnel();
+        personnel.setFirstname("sopon");
+        personnel.setLastname("phudee");
+        personnel.setTelephone("0856845611");
+        personnel.setPassword("12345648790");
+        personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
+        personnel = personnelRepository.saveAndFlush(personnel);
+
+
         Food name= new Food();
         name.setName("กกก");
+        name.setCreatedby(personnel);
+        name.setTypeby(type);
+        name.setMealby(meal);
 
         Set<ConstraintViolation<Food>> result = validator.validate(name);
         assertEquals(1, result.size());
@@ -75,8 +204,44 @@ public class Testfood {
     }
     @Test
     void B6021726_testSize21() {
+
+        Foodtype type = new Foodtype();
+        type.setType("อาหารธรรมดา");
+        type = foodtypeRepository.saveAndFlush(type);
+
+        Meal meal = new Meal();
+        meal.setMealtype("เช้า");
+        meal = mealRepository.saveAndFlush(meal);
+
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);        
+
+        Personnel personnel = new Personnel();
+        personnel.setFirstname("sopon");
+        personnel.setLastname("phudee");
+        personnel.setTelephone("0856845611");
+        personnel.setPassword("12345648790");
+        personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
+        personnel = personnelRepository.saveAndFlush(personnel);
+
+
         Food name = new Food();
         name.setName("กกกกกกกกกกกกกกกกกกกกก");
+        name.setCreatedby(personnel);
+        name.setTypeby(type);
+        name.setMealby(meal);
 
         Set<ConstraintViolation<Food>> result = validator.validate(name);
         assertEquals(1, result.size());
@@ -87,8 +252,44 @@ public class Testfood {
     }
     @Test
     void B6021726_testFoodMustNotBeDigi() {
+
+        Foodtype type = new Foodtype();
+        type.setType("อาหารธรรมดา");
+        type = foodtypeRepository.saveAndFlush(type);
+
+        Meal meal = new Meal();
+        meal.setMealtype("เช้า");
+        meal = mealRepository.saveAndFlush(meal);
+
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);        
+
+        Personnel personnel = new Personnel();
+        personnel.setFirstname("sopon");
+        personnel.setLastname("phudee");
+        personnel.setTelephone("0856845611");
+        personnel.setPassword("12345648790");
+        personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
+        personnel = personnelRepository.saveAndFlush(personnel);
+
+
         Food name = new Food();
         name.setName("ข้าวต้ม1");
+        name.setCreatedby(personnel);
+        name.setTypeby(type);
+        name.setMealby(meal);
         
         Set<ConstraintViolation<Food>> result = validator.validate(name);
         assertEquals(1, result.size());
@@ -96,6 +297,125 @@ public class Testfood {
         ConstraintViolation<Food> v = result.iterator().next();
         assertEquals("must match \"\\D{4,20}\"", v.getMessage());
         assertEquals("name",v.getPropertyPath().toString());
+    }
+     @Test
+    void B6021726_Personnel_Combobox_MustNotBeNull(){
+
+        Foodtype type = new Foodtype();
+        type.setType("อาหารธรรมดา");
+        type = foodtypeRepository.saveAndFlush(type);
+
+        Meal meal = new Meal();
+        meal.setMealtype("เช้า");
+        meal = mealRepository.saveAndFlush(meal);
+
+        Food name = new Food();
+        name.setName("ข้าวต้ม");
+        name.setCreatedby(null);
+        name.setTypeby(type);
+        name.setMealby(meal);
+
+        Set<ConstraintViolation<Food>> result = validator.validate(name);
+        assertEquals(1, result.size());
+
+        ConstraintViolation<Food> v = result.iterator().next();
+        assertEquals("must not be null", v.getMessage());
+        assertEquals("createdby", v.getPropertyPath().toString());
+
+    }
+    @Test
+    void B6021726_Foodtype_Combobox_MustNotBenull(){
+        Foodtype type = new Foodtype();
+        type.setType("อาหารธรรมดา");
+        type = foodtypeRepository.saveAndFlush(type);
+
+        Meal meal = new Meal();
+        meal.setMealtype("เช้า");
+        meal = mealRepository.saveAndFlush(meal);
+
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);        
+
+        Personnel personnel = new Personnel();
+        personnel.setFirstname("sopon");
+        personnel.setLastname("phudee");
+        personnel.setTelephone("0856845611");
+        personnel.setPassword("12345648790");
+        personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
+        personnel = personnelRepository.saveAndFlush(personnel);
+        
+
+        Food name = new Food();
+        name.setName("ข้าวต้ม");
+        name.setCreatedby(personnel);
+        name.setTypeby(null);
+        name.setMealby(meal);
+
+        Set<ConstraintViolation<Food>> result = validator.validate(name);
+        assertEquals(1, result.size());
+
+        ConstraintViolation<Food> v = result.iterator().next();
+        assertEquals("must not be null", v.getMessage());
+        assertEquals("typeby", v.getPropertyPath().toString());
+    }
+        @Test
+    void B6021726_Meal_Combobox_MustNotBenull(){
+        Foodtype type = new Foodtype();
+        type.setType("อาหารธรรมดา");
+        type = foodtypeRepository.saveAndFlush(type);
+
+        Meal meal = new Meal();
+        meal.setMealtype("เช้า");
+        meal = mealRepository.saveAndFlush(meal);
+
+        Educationlevel level = new Educationlevel();
+        level.setEducationlevel_name("Bachelor of Arts");
+        level = educationlevelRepository.saveAndFlush(level);
+
+        Maritalstatus status = new Maritalstatus();
+        status.setMaritalstatus_name("single");
+        status = maritalstatusRepository.saveAndFlush(status);
+
+        Position posit = new Position();
+        posit.setPosition_name("Doctor");
+        posit = positionRepository.saveAndFlush(posit);        
+
+        Personnel personnel = new Personnel();
+        personnel.setFirstname("sopon");
+        personnel.setLastname("phudee");
+        personnel.setTelephone("0856845611");
+        personnel.setPassword("12345648790");
+        personnel.setBirthday(LocalDate.parse("1999-01-29"));
+        personnel.setStatus(status);
+        personnel.setPosit(posit);
+        personnel.setLevel(level);
+        personnel = personnelRepository.saveAndFlush(personnel);
+        
+
+        Food name = new Food();
+        name.setName("ข้าวต้ม");
+        name.setCreatedby(personnel);
+        name.setTypeby(type);
+        name.setMealby(null);
+
+        Set<ConstraintViolation<Food>> result = validator.validate(name);
+        assertEquals(1, result.size());
+
+        ConstraintViolation<Food> v = result.iterator().next();
+        assertEquals("must not be null", v.getMessage());
+        assertEquals("mealby", v.getPropertyPath().toString());
     }
 
 }
